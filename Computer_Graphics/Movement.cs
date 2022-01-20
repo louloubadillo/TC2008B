2af547eb1bfea7b521d12f0eca9de0b1be22f135
louloubadillo <lulu.badillo.csm@gmail.com>
2022-01-20 09:46:10 -0600

    add movement scripts

---
+++ Computer_Graphics/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    Vector3 tempPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tempPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        tempPos.x += 0.5f;
+
+        tempPos.z += 0.5f;
+
+        transform.position = tempPos;
+
+        if (tempPos.x > 20){
+            enabled = false;
+        }
+    }
+}
+
